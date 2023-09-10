a3a92ac5b92bbb27b4d61ce89b2e62056095045a
Suprajna Sharma <suprasubedi11@gmail.com>
2023-09-10 13:05:43 +0545

    view single employee completed

--- controller/employees.controller.js
+++ controller/employees.controller.js
@@ -1,14 +1,17 @@
-import { getAllUsersDb } from "../services/employee.services.js";
+import { getAllEmpsDb, getOneEmpsDb } from "../services/employee.services.js";
 
 export const viewAllEmployees = async (req, res, next) => {
-  const emps = await getAllUsersDb();
-  console.log(emps);
+  const emps = await getAllEmpsDb();
+  // console.log(emps);
   return res.send(emps);
 };
 
 export const viewOneEmployee = async (req, res, next) => {
-  console.log("view one emps");
-  res.send("view one employees");
+  const { id } = req.params;
+  const emp = await getOneEmpsDb(parseInt(id));
+  // console.log(emp);
+  if (emp === null) return res.status(404).send("no records found");
+  return res.send(emp);
 };
 
 export const createEmployee = async (req, res, next) => {
--- services/employee.services.js
+++ services/employee.services.js
@@ -1,16 +1,31 @@
 import prisma from "./index.database.js";
 
 //get all users
-
-export const getAllUsersDb = async () => {
+export const getAllEmpsDb = async () => {
   try {
     const allUsers = await prisma.employee.findMany({
       orderBy: {
-        createdAt: "desc",
+        entry_date: "desc",
       },
     });
     return allUsers;
   } catch (error) {
     console.log(error);
+    return res.status(500).send(error);
+  }
+};
+
+//get user by Id
+export const getOneEmpsDb = async (employee_id) => {
+  try {
+    const emp = await prisma.employee.findUnique({
+      where: {
+        employee_id,
+      },
+    });
+    return emp;
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
   }
 };
